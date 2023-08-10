4386ad3b1a6a6606b850f34d4593e162dba44907
maskudo <manujkarki101@gmail.com>
2023-08-10 21:48:45 +0545

    feat: add test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    browser: true,
+    node: true,
     es6: true,
     "jest/globals": true,
   },
@@ -25,6 +25,5 @@ module.exports = {
     "no-console": "error",
     "react/prop-types": 0,
     "no-console": "off",
-    "no-undef": "off",
   },
 };
